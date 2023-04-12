921cec93fdb606e42471bb240613bd804da29c68
Yashank <Yashank.Tiwari@GeminiSolutions.com>
2023-04-12 17:38:54 +0530

    Added Navbar, Logo and Background Image

--- frontend/src/Components/CommonNavbar.js
+++ frontend/src/Components/CommonNavbar.js
@@ -1,8 +1,10 @@
 const CommonNavbar = () => {
+
     return (
         <>
             {/* Write the html code here */}
-            <h1>Hello from CommonNavbar</h1>
+            {/*<h1>Hello from CommonNavbar</h1>*/}
+            <h1>Hello from Common Navbar</h1>
         </>
     )
 };
--- frontend/src/Components/HomeNavbar.js
+++ frontend/src/Components/HomeNavbar.js
@@ -1,8 +1,34 @@
+import {FaBars, FaTimes} from "react-icons/fa";
+import {useRef} from "react";
+import '../css/HomeNavbar.css';
+import logo from '../assets/img/estately-logo.png';
+import { Link } from 'react-router-dom';
+
 const HomeNavbar = () => {
+
+    const navRef = useRef();
+    const showNavbar = () =>{
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <>
             {/* Write the html code here */}
-            <h1>Hello from HomeNavbar</h1>
+            <header className="flex items-center">
+                <img src={logo} alt="Logo" className="h-[3rem] w-[10rem]"/>
+                <nav ref = {navRef} className="pb-4">
+                    <Link to="/">Home</Link>
+                    <a href="/#">Contact Us!</a>
+                    <Link to="/aboutus">About Us!</Link>
+
+                    <button className = "nav-btn nav-close-btn" onClick={showNavbar}>
+                        <FaTimes />
+                    </button>
+                </nav>
+                <button className = "nav-btn" onClick={showNavbar}>
+                    <FaBars />
+                </button>
+            </header>
         </>
     )
 };
