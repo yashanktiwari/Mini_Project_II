ecb93f42d11b855aa26b255a395bde64cdcf9c1e
Yashank <Yashank.Tiwari@GeminiSolutions.com>
2023-04-13 14:03:08 +0530

    Updated Redux Store

--- frontend/src/utils/userSlice.js
+++ frontend/src/utils/userSlice.js
@@ -3,16 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: 'User',
     initialState: {
-        // isLoggedIn: false,
-        // userid: "",
-        // username: ""
-        user: {}
+        user: {},
     },
     reducers: {
         setUser: (state, action) => {
-            // state.isLoggedIn = true;
-            // state.username = action.payload.username;
-            // state.userid = action.payload.userid;
             state.user = action.payload;
         },
     },
