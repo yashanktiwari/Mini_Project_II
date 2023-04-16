a81acc0fcc14c4412d9bda99f7a667d240f0a3cd
AnujRuhela7 <ruhela7777@gmail.com>
2023-04-16 16:31:07 +0530

    Added footer in Contact Us page

--- frontend/src/Pages/ContactUs.js
+++ frontend/src/Pages/ContactUs.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import CommonNavbar from "../Components/CommonNavbar";
+import Footer from "../Components/Footer";
 
 const ContactUs = () => {
 
@@ -73,6 +74,7 @@ const ContactUs = () => {
                     </form>
                 </div>
             </section>
+            <Footer/>
         </>
     )
 };
