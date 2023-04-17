02039e1688f3bf4d940429a65343f360f0c8ca0a
Yashank <Yashank.Tiwari@GeminiSolutions.com>
2023-04-17 23:21:54 +0530

    Carousel component

---
+++ frontend/src/Components/SinglePageCarousel.js
@@ -0,0 +1,78 @@
+import {useState} from "react";
+
+
+const SinglePageCarousel = ({images}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === images.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+    return (
+        <>
+            <div className='w-[45rem] h-[50rem] m-auto py-16 px-4 relative group'>
+                <div
+                    style={{ backgroundImage: `url(${images[currentIndex]})` }}
+                    className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+                ></div>
+                {/* Left Arrow */}
+                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <span className="inline-block h-8 w-8" onClick={prevSlide}>
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-8 w-8">
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"/>
+      </svg>
+    </span>
+                </div>
+                {/* Right Arrow */}
+                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <span className="inline-block h-8 w-8" onClick={nextSlide}>
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-8 w-8">
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
+      </svg>
+    </span>
+                </div>
+                <div className='flex top-4 justify-center py-2'>
+                    {images.map((slide, slideIndex) => (
+                        <div
+                            key={slideIndex}
+                            onClick={() => goToSlide(slideIndex)}
+                            className='text-2xl cursor-pointer'
+                        >
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default SinglePageCarousel;
