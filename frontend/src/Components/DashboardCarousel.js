cd5f48d5277579b0d70473ed34b4e25ceaed0233
AnujRuhela7 <ruhela7777@gmail.com>
2023-04-22 14:56:05 +0530

    Added carousel on dashboard

---
+++ frontend/src/Components/DashboardCarousel.js
@@ -0,0 +1,82 @@
+import React, {useState} from "react";
+import {BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs";
+import {RxDotFilled} from "react-icons/rx";
+
+const DashboardCarousel = () => {
+    const slides = [
+        {
+            url: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/3288100/pexels-photo-3288100.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/3288102/pexels-photo-3288102.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/534151/pexels-photo-534151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/1571463/pexels-photo-1571463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/2343465/pexels-photo-2343465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        },
+        {
+            url: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        }
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    }
+
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }
+
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    }
+
+    return (
+        <>
+            <div className="max-w-[1450px] h-[350px] pb-8 w-full m-auto relative group">
+                <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className="w-full h-full bg-center bg-cover duration-500">
+                </div>
+
+                {/*Left Arrow*/}
+                <div className="hidden group-hover:block hover:bg-gray-300/10 hover:text-white absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                    <BsChevronCompactLeft onClick={prevSlide} size={30}/>
+                </div>
+                {/*Right Arrow*/}
+                <div className="hidden group-hover:block hover:bg-gray-300/10 hover:text-white absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                    <BsChevronCompactRight onClick={nextSlide} size={30}/>
+                </div>
+                <div className="flex top-4 justify-center py-2">
+                    {
+                        slides.map((slide, slideIndex) => (
+                            <div key={slideIndex} onClick={() => goToSlide(slideIndex) } className="text-2xl cursor-pointer ">
+                                <RxDotFilled/>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </>
+    )
+ }
+
+ export default DashboardCarousel;
--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -4,6 +4,8 @@ import {useEffect, useState} from "react";
 import CommonNavbar from "../Components/CommonNavbar";
 import verifyToken from "../utils/verifyToken";
 import axios from "axios";
+// import FilterBox from "../Components/FilterBox";
+import DashboardCarousel from "../Components/DashboardCarousel";
 
 const Dashboard = () => {
 
@@ -44,31 +46,53 @@ const Dashboard = () => {
     return (
         <>
             <CommonNavbar />
-            <h1>Dashboard</h1>
-            <h1>User is logged in</h1>
-            <h1>The email is: {userStore.user.email}</h1>
-            <h1>The userid is: {userStore.user._id}</h1>
-            <h1>The role is: {userStore.user.role}</h1>
+            {/*<h1>Dashboard</h1>*/}
+            {/*<h1>User is logged in</h1>*/}
+            {/*<h1>The email is: {userStore.user.email}</h1>*/}
+            {/*<h1>The userid is: {userStore.user._id}</h1>*/}
+            {/*<h1>The role is: {userStore.user.role}</h1>*/}
 
-            <div className="flex">
-            {properties &&
-                properties.map((property) => {
-                    return (
+            {/*<FilterBox/>*/}
 
-                        <Link to={`/properties/${property._id}`}
-                           className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 bg-gray-800 dark:hover:bg-gray-700 mx-2">
-                            <img
-                                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                                src={property.primary_img} alt="Property_Image" />
-                                <div className="flex flex-col justify-between p-4 leading-normal">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{property.title}</h5>
-                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{property.description}</p>
+            <DashboardCarousel/>
+
+
+            <div className="grid grid-cols-2 gap-12 mx-12 mt-6">
+
+                {properties &&
+                        properties.map((property) => {
+                            return (
+                                <div className="rounded-[7px] mb-4">
+                                    <Link to={`/properties/${property._id}`}
+                                       className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                        <img
+                                            className="object-cover w-full rounded-t-lg h-[100%] md:h-[100%] md:w-2/5 md:rounded-none md:rounded-l-lg"
+                                            src={property.primary_img} alt="Property_Image" />
+                                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{property.title}</h5>
+                                                <span >
+                                                    <p className="text-lg inline font-semibold tracking-tight text-white">Owner :</p>
+                                                    <p className="inline text-white tracking-wide">{}</p>
+                                                </span>
+                                                <span>
+                                                    <p className="text-lg inline font-semibold tracking-tight text-white">Address : </p>
+                                                    <p className="inline text-white tracking-wide">{property.address}</p>
+                                                </span>
+                                                <span>
+                                                    <p className="text-lg inline font-semibold tracking-tight text-white">Area : </p>
+                                                    <p className="inline text-white tracking-wide">{property.area} sq. ft.</p>
+                                                </span>
+                                                <span>
+                                                    <p className="text-lg inline font-semibold tracking-tight text-white">Price : </p>
+                                                    <p className="inline text-white tracking-wide">Rs. {property.price}</p>
+                                                </span>
+                                            </div>
+                                    </Link>
                                 </div>
-                        </Link>
+                            )
+                        })
+                    }
 
-                )
-                })
-            }
             </div>
 
             {/*<button onClick={handleLogout}>Logout</button>*/}
