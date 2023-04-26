64d7e998810878afb3573bc25434520c7ffdc617
Yashank <Yashank.Tiwari@GeminiSolutions.com>
2023-04-26 23:44:55 +0530

    Fixed both the navbars

--- frontend/src/Components/HomeNavbar.js
+++ frontend/src/Components/HomeNavbar.js
@@ -1,33 +1,92 @@
-import {FaBars, FaTimes} from "react-icons/fa";
-import {useRef} from "react";
+import {Fragment, useRef} from "react";
 import '../css/HomeNavbar.css';
 import logo from '../assets/img/estately-logo.png';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {Menu, Transition} from "@headlessui/react";
+import {BsList} from "react-icons/bs";
 
 const HomeNavbar = () => {
 
-    const navRef = useRef();
-    const showNavbar = () =>{
-        navRef.current.classList.toggle("responsive_nav");
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
     }
 
     return (
         <>
             {/* Write the html code here */}
-            <header className="flex items-center">
+            <header className="flex sticky top-0 w-full">
                 <img src={logo} alt="Logo" className="h-[3rem] w-[10rem]"/>
-                <nav ref = {navRef} className="pb-4">
+
+                <div className={"min-[300px]:hidden md:hidden min-w-[30rem] lg:flex justify-between"}>
                     <Link to="/">Home</Link>
                     <Link to="/contactus">Contact Us</Link>
                     <Link to="/aboutus">About Us</Link>
+                </div>
 
-                    <button className = "nav-btn nav-close-btn" onClick={showNavbar}>
-                        <FaTimes />
-                    </button>
-                </nav>
-                <button className = "nav-btn" onClick={showNavbar}>
-                    <FaBars />
-                </button>
+                <div className={"w-[5rem] bg-transparent"}></div>
+                <Menu as="div" className="min-[300px]:inline-block lg:hidden relative text-left">
+                    <div>
+                        <Menu.Button
+                            className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-gray-100 shadow-sm">
+                            <BsList className="-mr-1 h-7 w-7 text-gray-100" aria-hidden="true"/>
+                        </Menu.Button>
+                    </div>
+
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items
+                            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-b-md bg-black shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({active}) => (
+                                        <Link
+                                            to={'/'}
+                                            className={classNames(
+                                                active ? 'bg-gray-500 text-gray-100' : 'text-gray-100',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Home
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({active}) => (
+                                        <Link
+                                            to={'/contactus'}
+                                            className={classNames(
+                                                active ? 'bg-gray-500 text-gray-100' : 'text-gray-100',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Contact Us
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({active}) => (
+                                        <Link
+                                            to={'/aboutus'}
+                                            className={classNames(
+                                                active ? 'bg-gray-500 text-gray-100' : 'text-gray-100',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            About Us
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </header>
         </>
     )
